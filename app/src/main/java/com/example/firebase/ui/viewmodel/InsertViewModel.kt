6b2b2bf3a38c8dcbe9a531e3a406b51588f44d05
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-01-12 02:26:21 +0700

    membuat data clas MahasiswaEvent

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.firebase.ui.viewmodel
+
+import com.example.firebase.model.Mahasiswa
+
+data class MahasiswaEvent(
+
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
+
