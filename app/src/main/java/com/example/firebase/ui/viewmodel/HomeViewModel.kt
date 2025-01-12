dda336df6d8247c38bf182ceabebfa7de0005c75
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-01-12 14:14:20 +0700

    menambahkan fungsi delete mhs di home viewmodel

--- app/src/main/java/com/example/firebase/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/HomeViewModel.kt
@@ -45,4 +45,15 @@ class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel() {
                 }
         }
     }
+    fun deleteMhs(mahasiswa: Mahasiswa){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(mahasiswa)
+            } catch (e: Exception){
+                mhsUIState = HomeUiState.Error(e)
+            }
+        }
+    }
 }
+
+
